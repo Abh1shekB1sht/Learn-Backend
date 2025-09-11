e3134a90cbc47e4745ef1e5f9d1700fb30199fe0
Abhishek <singhbisht84484@gmail.com>
2025-09-11 16:15:35 +0530

    created usermodel

---
+++ src/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+  watchHistory: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: 'video'
+    }
+  ],
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true,
+  },
+  avatar: {
+    type: String, // cloudinary url 
+    required: true,
+  },
+  coverImage: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  refreshToken: {
+    type: String,
+  }
+}, {timestamps: true})
+
+export const User = mongoose.model('User', userSchema)
