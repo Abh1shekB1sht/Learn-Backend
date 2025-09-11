4d62a41992b1a5ecb55324931e54a9d3b0e611e9
Abhishek <singhbisht84484@gmail.com>
2025-09-11 16:20:53 +0530

    made some changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
   watchHistory: [
     {
-      type: mongoose.Types.ObjectId,
+      type: Schema.Types.ObjectId,
       ref: 'video'
     }
   ],
